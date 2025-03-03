85888985e5d6f188d206260627ec836fd2c56179
aldaldaldald <183981294+aldaldaldald@users.noreply.github.com>
2025-03-03 13:07:32 +0000

    Setup Expo and navigation bar

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,79 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  // state in here to decide what to render, e.g is onboarding complete? (only needs to be done once)
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Habits",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="list.bullet" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="stats"
+        options={{
+          title: "Stats",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="chart.bar.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="pet"
+        options={{
+          title: "Pet",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="pawprint.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="shop"
+        options={{
+          title: "Shop",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="basket.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="friends"
+        options={{
+          title: "Friends",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="person.3.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/onboarding/onboarding-1.tsx
@@ -0,0 +1,107 @@
+import {
+  Text,
+  View,
+  Button,
+  Alert,
+  StyleSheet,
+  Image,
+  TextInput,
+  FlatList,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import React from "react";
+export default function Index() {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState("");
+  const images = [
+    require("../../assets/images/cat.webp"),
+    require("../../assets/images/dog.jpg"),
+    require("../../assets/images/owl.jpg"),
+  ];
+  const [character, setCharacter] = React.useState(0);
+  const changeCharacter = () => {
+    if (character == 2) {
+      setCharacter(0);
+    } else {
+      setCharacter(character + 1);
+    }
+  };
+  return (
+    <SafeAreaProvider
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <SafeAreaView
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ fontSize: 25, fontWeight: 600 }}>Habigotchi</Text>
+        <Text style={{ margin: 30, fontSize: 15 }}>
+          Select and name your pet
+        </Text>
+        <SafeAreaView
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            title="<----  "
+            color="#F194FF"
+            onPress={() => {
+              changeCharacter();
+            }}
+          />
+          <Image
+            id="character-image"
+            source={images[character]}
+            style={{ width: 170, height: 170 }}
+          ></Image>
+          <Button
+            title="---->"
+            color="#F194FF"
+            onPress={() => {
+              changeCharacter();
+            }}
+          />
+        </SafeAreaView>
+      </SafeAreaView>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          placeholder="enter pet name..."
+          placeholderTextColor="gray"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="write a profile status..."
+          placeholderTextColor="gray"
+        />
+        <Button
+          title="Save"
+          color="#F194FF"
+          onPress={() => Alert.alert("Button with adjusted color pressed")}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: 160,
+    margin: 6,
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 10,
+    alignSelf: "center",
+    color: "red",
+  },
+});
