dc4e3b19f42e9be729389ea132ab5a4bc4d5dc6f
Jfloyd7720 <floyd.latc@gmail.com>
2025-03-04 22:15:39 +0000

    add images and update shop file

--- app/(tabs)/shop.tsx
+++ app/(tabs)/shop.tsx
@@ -1,4 +1,11 @@
-import { StyleSheet, Image, Platform } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  Platform,
+  View,
+  Text,
+  ScrollView,
+} from "react-native";
 
 import { Collapsible } from "@/components/Collapsible";
 import { ExternalLink } from "@/components/ExternalLink";
@@ -6,36 +13,162 @@ import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { IconSymbol } from "@/components/ui/IconSymbol";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Shop() {
+  const items = [
+    {
+      id: "1",
+      name: "Re-energizer",
+      price: "100 coins",
+      img_url: require("../../assets/images/plug.png"),
+      health: "50",
+      happiness: "20",
+    },
+    {
+      id: "2",
+      name: "Dog Bone",
+      price: "150 coins",
+      img_url: require("../../assets/images/dog-bone.png"),
+      health: "50",
+      happiness: "20",
+    },
+    {
+      id: "3",
+      name: "Doe",
+      price: "150 coins",
+      img_url: require("../../assets/images/dog-bone.png"),
+      health: "50",
+      happiness: "20",
+    },
+  ];
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="basket.fill"
-          style={styles.headerImage}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Shop</ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={{ flex: 1 }}>
+      {" "}
+      <View
+        style={{
+          flexDirection: "row",
+
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={require("../../assets/images/health-icon.png")}
+          style={{
+            margin: 10,
+            width: 40,
+            height: 40,
+            alignSelf: "center",
+            justifyContent: "center",
+          }}
+        ></Image>
+
+        <Image
+          source={require("../../assets/images/coin.png")}
+          style={{
+            margin: 10,
+            width: 50,
+            height: 50,
+            alignSelf: "center",
+            justifyContent: "center",
+          }}
+        ></Image>
+        <Image
+          source={require("../../assets/images/happiness.png")}
+          style={{
+            margin: 10,
+            width: 50,
+            height: 50,
+            alignSelf: "center",
+            justifyContent: "center",
+          }}
+        ></Image>
+      </View>
+      <Image
+        style={{
+          width: 170,
+          height: 150,
+          alignSelf: "center",
+          justifyContent: "center",
+        }}
+        source={require("../../assets/images/character.png")}
+      ></Image>
+      <Text
+        style={{ margin: 30, fontSize: 55, fontWeight: 600, color: "white" }}
+      >
+        SHOP
+      </Text>
+      <ScrollView contentContainerStyle={{ paddingBottom: 0 }}>
+        {items.map((item) => {
+          return (
+            <View
+              key={item.id}
+              style={{
+                display: "flex",
+
+                alignItems: "center",
+                margin: 10,
+              }}
+            >
+              <Text style={{ color: "white", fontSize: 20, marginBottom: 10 }}>
+                {item.name}
+              </Text>
+              <Image
+                style={{ width: 80, height: 80 }}
+                source={item.img_url}
+              ></Image>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "flex-end",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <View style={{ alignItems: "center" }}>
+                  <Image
+                    source={require("../../assets/images/health-icon.png")}
+                    style={{
+                      margin: 10,
+                      width: 30,
+                      height: 30,
+                      alignSelf: "center",
+                      justifyContent: "center",
+                    }}
+                  ></Image>
+                  <Text style={{ color: "white" }}>{item.health}</Text>
+                </View>
+                <View style={{ alignItems: "center" }}>
+                  <Image
+                    source={require("../../assets/images/coin.png")}
+                    style={{
+                      margin: 10,
+                      width: 30,
+                      height: 30,
+                      alignSelf: "center",
+                      justifyContent: "center",
+                    }}
+                  ></Image>
+                  <Text style={{ color: "white" }}>{item.price}</Text>
+                </View>
+                <View style={{ alignItems: "center" }}>
+                  <Image
+                    source={require("../../assets/images/happiness.png")}
+                    style={{
+                      margin: 10,
+                      width: 30,
+                      height: 30,
+                      alignSelf: "center",
+                      justifyContent: "center",
+                    }}
+                  ></Image>
+                  <Text style={{ color: "white" }}>{item.happiness}</Text>
+                </View>
+              </View>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
-  },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
-  },
-});
